080061bfa8fc84b59aa2f52989ff46e843168c59
BuschMath <BuschMath@users.noreply.github.com>
2020-10-20 13:36:12 -0500

    Continue Break Example

--- Ch7_Example/Ch7_BreakContinue_Example.cpp
+++ Ch7_Example/Ch7_BreakContinue_Example.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-/*	for (int i = 1; i <= 10; i++)
+	for (int i = 1; i <= 10; i++)
 	{
 		cout << i << endl;
 		for (int j = 1; j <= 10; j++)
@@ -17,14 +17,14 @@ int main()
 			cout << "\t" << j << endl;
 		}
 	}
-*/
-	int i = 0;
+
+/*	int i = 0;
 	do
 	{
 		cout << i++ << endl;
 		continue;
 		cout << i << endl;
 	} while (i<10);
-
+*/
 	return 0;
 }
